d10fa55cb79a6fafaa73223121e9fee3b2d5f86b
dmitriyandriyanov <garro93@gmail.com>
2022-01-27 22:40:48 +0300

    Hash map resize in progress

--- src/bytecode/hash_map/mod.rs
+++ src/bytecode/hash_map/mod.rs
@@ -1,6 +1,10 @@
+mod raw_table;
+
 use super::vec::RawVec;
 use super::value::Value;
+use raw_table::{RawTable, Hashable, Entry};
 use std::cmp::Eq;
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::ptr;
 
@@ -8,27 +12,19 @@ const TABLE_MAX_LOAD: f32 = 0.75;
 
 pub struct HashTable<Key: Hashable + Eq, Value> {
     length: usize,
-    buffer: RawVec<Entry<Key, Value>>,
-}
-
-pub trait Hashable {
-    fn hash(&self) -> usize;
-}
-
-struct Entry<Key: Eq, Value> {
-    key: Option<Key>,
-    value: Value,
+    buffer: RawTable<Key, Value>,
 }
 
 impl<Key: Hashable + Eq + Debug, Value: Debug + Default> HashTable<Key, Value> {
     pub fn new() -> Self {
         Self {
             length: 0,
-            buffer: RawVec::new(),
+            buffer: RawTable::new(),
         }
     }
     pub fn insert(&mut self, key: Key, value: Value) {
         self.grow_if_needed();
+        self.length += 1;
         let index = key.hash() % self.buffer.capacity;
         unsafe {
             let mut pointer = self.pointer().add(index);
@@ -53,20 +49,14 @@ impl<Key: Hashable + Eq + Debug, Value: Debug + Default> HashTable<Key, Value> {
     pub fn find(&self, key: &Key) -> Option<&Value> {
         let mut index = key.hash() % self.buffer.capacity;
         let initial_index = index;
+        println!("index {}", initial_index);
         loop {
+            println!("next index {}", index);
             unsafe {
-                let entry = self.pointer().add(index).as_ref().unwrap();
-                match &entry.key {
-                    Some(entry_key) if key == entry_key => {
-                        return Some(&entry.value);
-                    },
-                    _ => {
-                        index = (index + 1) % self.buffer.capacity;
-                    }
-                }
-                if initial_index == index {
-                    return None;
-                }
+                // println!("pointer: {:?}", self.pointer().add(index));
+                // let entry = self.pointer().add(index);
+                // println!("entry {:?}", entry.key);
+                return None;
             };
         }
     }
@@ -76,17 +66,9 @@ impl<Key: Hashable + Eq + Debug, Value: Debug + Default> HashTable<Key, Value> {
     }
 
     fn grow_if_needed(&mut self) {
-        if self.length + 1 <= self.buffer.capacity * 75 / 100 {
-            return;
-        }
-        self.buffer.grow();
-        for index in 0..self.buffer.capacity + 1 {
-            unsafe {
-                ptr::write(self.pointer().add(index), Entry {
-                    key: None,
-                    value: Value::default()
-                })
-            }
+        println!("grow if needed {} - {}", self.buffer.capacity, self.buffer.capacity * 75 / 100);
+        if self.length + 1 > self.buffer.capacity * 75 / 100 {
+            self.buffer.grow();
         }
     }
 
@@ -113,6 +95,18 @@ mod tests {
         hash_map.insert("some_key".to_string(), Value::Bool(true));
         let value = hash_map.find(&"some_key".to_string()).map(|v| v.clone());
         assert_eq!(value, Some(Value::Bool(true)));
+    }
+
+    #[test]
+    fn resize() {
+        let mut hash_map = HashTable::<String, Value>::new();
+        let first_key = "some_key".to_string();
+        let second_key = "other_key".to_string();
+        hash_map.insert(first_key.clone(), Value::Bool(true));
+        hash_map.insert(second_key.clone(), Value::Number(1f32));
+        hash_map.insert("other_key_3".to_string(), Value::Number(1f32));
+        // assert_eq!(hash_map.contains(&first_key), true);
+        // assert_eq!(hash_map.contains(&"other_key".to_string()), true)
     }
 
     impl Hashable for String {
---
+++ src/bytecode/hash_map/raw_table.rs
@@ -0,0 +1,98 @@
+use std::alloc::{self, Layout};
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+use std::cmp::Eq;
+use std::ptr;
+use std::mem;
+
+pub struct RawTable<Key: Eq, Value> {
+    pub pointer: NonNull<Entry<Key, Value>>,
+    pub capacity: usize,
+    _marker: PhantomData<Entry<Key, Value>>
+}
+
+pub trait Hashable {
+    fn hash(&self) -> usize;
+}
+
+pub struct Entry<Key: Eq, Value> {
+    pub key: Option<Key>,
+    pub value: Value,
+}
+
+impl<Key: Hashable + Eq, Value: Default> RawTable<Key, Value> {
+    pub fn new() -> Self {
+        RawTable {
+            pointer: NonNull::dangling(),
+            capacity: 0,
+            _marker: PhantomData
+        }
+    }
+
+    pub fn grow(&mut self) {
+        let (new_capacity, new_layout) = if self.capacity == 0 {
+            (1, Layout::array::<Entry<Key, Value>>(1).unwrap())
+        } else {
+            let new_capacity = self.capacity * 2;
+            let new_layout = Layout::array::<Entry<Key, Value>>(new_capacity).unwrap();
+            (new_capacity, new_layout)
+        };
+        assert!(new_layout.size() <= isize::MAX as usize, "Allocation too large");
+
+        let new_pointer = unsafe {
+            let new_pointer = alloc::alloc(new_layout) as *mut Entry<Key, Value>;
+            RawTable::fill_new_table(new_pointer, new_capacity);
+            if self.capacity > 0 {
+                self.move_items_to_new_table(&new_pointer, new_capacity);
+            }
+            new_pointer
+        };
+
+        self.pointer = match NonNull::new(new_pointer) {
+            Some(p) => p,
+            None => alloc::handle_alloc_error(new_layout)
+        };
+        self.capacity = new_capacity;
+    }
+
+    unsafe fn fill_new_table(new_pointer: *mut Entry<Key, Value>, new_capacity: usize) {
+        for index in 0..new_capacity + 1 {
+            ptr::write(new_pointer.add(index), Entry {
+                key: None,
+                value: Value::default()
+            })
+        }
+    }
+
+    unsafe fn move_items_to_new_table(&self, new_pointer: &*mut Entry<Key, Value>, new_capacity: usize) {
+        assert!(new_capacity > self.capacity);
+        let old_pointer = self.pointer.as_ptr();
+        let mut index = 0usize;
+        let entry_size = mem::size_of::<Entry<Key, Value>>();
+        while index < self.capacity {
+            let entry = old_pointer.add(index).as_ref().unwrap();
+            index += 1;
+            match &entry.key {
+                None => {
+                    continue;
+                },
+                Some(key) => {
+                    let new_index = key.hash() % new_capacity;
+                    println!("debug replace at {} to {}", index - 1, new_index);
+                    new_pointer.add(new_index).copy_from_nonoverlapping(entry, entry_size);
+                }
+            }
+        }
+    }
+}
+
+impl<Key: Eq, Value> Drop for RawTable<Key, Value> {
+    fn drop(&mut self) {
+        if self.capacity != 0 {
+            let layout = Layout::array::<Entry<Key, Value>>(self.capacity).unwrap();
+            unsafe {
+                alloc::dealloc(self.pointer.as_ptr() as *mut u8, layout);
+            }
+        }
+    }
+}
